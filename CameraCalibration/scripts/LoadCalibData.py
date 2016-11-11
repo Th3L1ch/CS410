c37242db67f3401e22a3743ae0e2375f9a1de2a5
Conor <conorkiernan@live.ie>
2016-11-11 20:00:37 +0000

    final version

--- CameraCalibration/scripts/LoadCalibData.py
+++ CameraCalibration/scripts/LoadCalibData.py
@@ -8,15 +8,15 @@ from mpl_toolkits.mplot3d import axes3d
 
 data = np.loadtxt('data.txt')
 
-#fig = plt.figure()
-#ax = fig.gca(projection="3d")
-#ax.plot(data[:,0], data[:,1], data[:,2],'k.')
+fig = plt.figure()
+ax = fig.gca(projection="3d")
+ax.plot(data[:,0], data[:,1], data[:,2],'k.')
 
 #fig = plt.figure()
 #ax = fig.gca()
 #ax.plot(data[:,3], data[:,4],'r.')
 
-#plt.show()
+plt.show()
 
 def calibrateCamera3D(data):
     x = data.shape[0]
@@ -68,22 +68,22 @@ def calibrateCamera3D(data):
     return final
 
 def visualiseCameraCalibration3D(data, P):
-    
+
     p = np.zeros((data.shape[0], 4))
     for s in range(0,data.shape[0]):
         p[s][0] = data[s][0]
         p[s][1] = data[s][1]
         p[s][2] = data[s][2]
-	    p[s][3] = 1
+        p[s][3] = 1
 
     p = p.transpose()
-    
+
     tx = np.dot(P, p)
 
     xy = np.zeros((data.shape[0],2))
     for s in range(0,data.shape[0]):
-	    xy[s][0] = (tx[0][s]/tx[2][s])
-	    xy[s][1] = (tx[1][s]/tx[2][s])
+        xy[s][0] = (tx[0][s]/tx[2][s])
+        xy[s][1] = (tx[1][s]/tx[2][s])
     fig = plt.figure()
     ax = fig.gca()
     ax.plot(data[:, 3], data[:, 4], 'r.', xy[:,0], xy[:,1], 'b.')
@@ -98,30 +98,42 @@ def evaluateCameraCalibration(data, P):
         p[s][0] = data[s][0]
         p[s][1] = data[s][1]
         p[s][2] = data[s][2]
-	    p[s][3] = 1
+        p[s][3] = 1
 
     p = p.transpose()
-    
+
     tx = np.dot(P, p)
 
     xy = np.zeros((data.shape[0],2))
     for s in range(0,data.shape[0]):
-	    xy[s][0] = (tx[0][s]/tx[2][s])
-	    xy[s][1] = (tx[1][s]/tx[2][s])
+        xy[s][0] = (tx[0][s]/tx[2][s])
+        xy[s][1] = (tx[1][s]/tx[2][s])
+        #print s
+        #print 'xy array:\t\tx={},\t\t\ty={}'.format(xy[s][0], xy[s][1])
+        #print 'data array:\t\tx={},\t\t\ty={}'.format(data[s][3], data[s][4])
+        #xs = xy[s][0] - data[s][3]
+        # ys = xy[s][1] - data[s][4]
+        #print '\t delx = {}\t\t dely = {}'.format(xs, ys)
+        #xs2 = xs**2
+        #ys2 = ys**2
+        #print '\t delx2 = {}\t\t dely2 = {}'.format(xs2, ys2)
+        #xs2ys2 = xs2+ys2
+        #print '\t delx2dely2 = {}'.format(xs2ys2)
+        #dst = np.sqrt(xs2ys2)
+        #print '\t dst = {}'.format(dst)
 
     f = np.zeros((data.shape[0],1))
     for s in range(0,data.shape[0]):
-	#f[s][0] = ((xy[s][0]-data[s][0])**2 + (xy[s][1]-data[s][1])**2)**0.5
-	x2 = (xy[s][0]-data[s][0])*(xy[s][0]-data[s][0])
-	y2 = (xy[s][1]-data[s][1])*(xy[s][1]-data[s][1])
-	x2y2= x2+y2
-	sqrt = x2y2**(1/2)
-	f[s][0] = sqrt
+         x2 = (xy[s][0]-data[s][3])*(xy[s][0]-data[s][3])
+         y2 = (xy[s][1]-data[s][4])*(xy[s][1]-data[s][4])
+         x2y2= x2+y2
+         dist = np.sqrt(x2y2)
+         f[s][0] = dist
 
-    print(np.mean(f))
-    print(np.var(f))
-    print(np.min(f))
-    print(np.max(f))
+    print'Mean:\n{}'.format(np.mean(f))
+    print'Variance:\n{}'.format(np.var(f))
+    print'Min:\n{}'.format(np.min(f))
+    print'Max:\n{}'.format(np.max(f))
     return
 
 P = calibrateCamera3D(data)
